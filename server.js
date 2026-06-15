d6006cde9fe7b8d64776a67362d65c3ae87987b5
RenatoFSantos <renato.insigne@gmail.com>
2026-06-15 11:31:49 -0300

    update baseUrl

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL || "https://media-server.midilabs.com.br";
+const BASE_URL = process.env.BASE_URL || "https://media-server.midilabs.com.br/uploads";
 
 app.use(cors());
 
